bb1240058149762e531bd5da6e2e1f59c140d8a4
Blake0v2 <dumboyonpc@outlook.com>
2025-08-13 22:02:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let current_raid = {
 
 // Raid times for each island in terms of minute range
 const raid_times = {
-    "dedu_island": { "start": 15, "end": 29 },
+    "dedu_island": { "start": 0, "end": 29 },
     "snow_island": { "start": 30, "end": 44 },
     "jungle_island": { "start": 15, "end": 29 },
 };
